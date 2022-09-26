d7809753b74d4deb39239a667dd2bea2e4b34cef
honey <1730564896@qq.com>
2022-09-26 19:47:47 +0800

    项目配置-搭建基础路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
+      </div>
+      {/* 配置路由规则 */}
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/home" component={Home}></Route>
+      </Switch>
+    </Router>
   )
 }
 
